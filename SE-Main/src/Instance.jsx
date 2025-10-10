2ab86e5b5e2f5a50308da88b6c3b058843c6f392
Numan Subhani <noumansubhani01@gmail.com>
2025-10-10 12:51:44 +0500

    Backend

--- SE-Main/src/Instance.jsx
+++
@@ -1,2 +0,0 @@
-// src/config.js
-export const BASE_URL = "http://127.0.0.1:8000/api/";
--- SE-Main/src/utils/instance.js
+++
@@ -1,4 +0,0 @@
-// src/utils/instance.js
-const BASE_URL = process.env.REACT_APP_BACKEND_BASE || "https://selectexposure.pythonanywhere.com/api/";
-
-export default BASE_URL;
